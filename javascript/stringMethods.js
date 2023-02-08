41a563bb894c94b39693ce9177606e041f8e1b16
poonambhapkar34 <poonambhapkar34@gmail.com>
2023-02-08 11:39:26 +0530

    string methods

---
+++ javascript/stringMethods.js
@@ -0,0 +1,8 @@
+var text = 'pooja       patil     ';
+console.log(text.length);
+console.log(text);
+var carNames = 'Audi tt444 Jaguar,mini Cupoj0sw mini ift jjjjjj mini jjjj ';
+let res = carNames.slice(2,6);
+console.log(res);
+let carNames2 = carNames.replace(/mini/g,'I20');
+console.log(carNames2);
