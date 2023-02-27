8971a0e9077c70ce5c8f522337c52bcbd1e26252
poonambhapkar34 <poonambhapkar34@gmail.com>
2023-02-27 21:10:28 +0530

    component creation

--- NovAngular/angular_novbatch/src/app/app.component.ts
+++ NovAngular/angular_novbatch/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core';  // import section : where we add dependency classes
 
-@Component({
+@Component({  //component directive section: metadata about component
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular_novbatch';
+export class AppComponent { //main component class : where we write code logic
+  title = 'angular_novbatch'; //properties
+  myName = 'poonam';
+  mySurname : string = 'patil';
+  mySurname2? : string; //syntax change 
+ // mySurname2 : string;
+
+  constructor(){  // constructor : just normal function but it will triger implicity by compiler
+      console.log('inside constuctor..');
+  }
+  
+  test(){
+    var xyz = 100;
+    this.mySurname2 = 'Jadhav';
+  }
+
+  test2(){
+    this.test();
+  }
+
 }
--- NovAngular/angular_novbatch/src/app/app.module.ts
+++ NovAngular/angular_novbatch/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [ //compo+dir+pipes
-    AppComponent
+    AppComponent,
+    UserComponent
   ],
   imports: [ //modules
     BrowserModule,
